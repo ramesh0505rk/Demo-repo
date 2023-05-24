b58430f943a2c8abba4f885b84b13455803b4cb7
unknown <ramesh0505rk@gmail.com>
2023-05-24 15:26:37 +0530

    changed the str

--- disp.java
+++ disp.java
@@ -1,7 +1,7 @@
 class disp{
     public static void main(String r[]){
         System.out.println("ramesh".length());
-        System.out.println("rk".length());
+        System.out.println("sv".length());
         System.out.println(6%2);
     }
 }
