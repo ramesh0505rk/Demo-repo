8f08d8be1b696703e7017d484b7c9eb85b7c6662
unknown <ramesh0505rk@gmail.com>
2023-05-24 15:24:08 +0530

    switched to div

--- disp.java
+++ disp.java
@@ -2,6 +2,6 @@ class disp{
     public static void main(String r[]){
         System.out.println("ramesh".length());
         System.out.println("rk".length());
-        System.out.println(6%2);
+        System.out.println(6/2);
     }
 }
